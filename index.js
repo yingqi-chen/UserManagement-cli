13aa6cc7012b18a22487d8b9abdfb2d6a97f615d
chanwkkk <chanwk18@hotmail.com>
2020-05-23 17:12:22 -0600

    clean up unnecessary codes in index.js

--- index.js
+++ index.js
@@ -8,31 +8,9 @@ const db = mongoose.connect('mongodb://localhost:27017/myImportantDates', {
     useUnifiedTopology: true, 
 });
 
-// import User MODEL
-const User = require('./model/user')
-
-// import user methods
-const {addUser,listAllUsers,findUserByEmail,updateUser,deleteUser} = require('./model_methods/user_methods')
 
 module.exports = db
-// testing newly added methods
-// const userOptions = {
-//     name: "user3",
-//     email: "email@ee.com",
-//     password: "123"
-// }
 
-// const newUserOptions = {
-//     name: "user4",
-//     email: "email@ee.com",
-//     password: "123"
-// }
-
-// let user = addUser(userOptions)
-// // listAllUsers()
-// // console.log(findUserByEmail("email@ee.com"))
-
-// console.log(deleteUser("email@ee.com"))
 
 
 
