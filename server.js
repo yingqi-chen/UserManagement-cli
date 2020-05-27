6471d3e63a156a7964a55efb3aa593761fbed462
chanwkkk <chanwk18@hotmail.com>
2020-05-27 16:51:35 -0600

    use express.static to add pics on website

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path')
+
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, './public')))
+
+app.get("/", (req,res) =>{
+  res.sendFile('/public/index.html',{ root : __dirname})
+})
+
 app.listen(PORT, () =>{
     console.log(`Listening on ${PORT}...`)
 } )
-
-app.get("/", (req,res) =>{
-  res.send("hi we change!!")
-})
