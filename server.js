ddda364ed14f92cadd2ce08ad1f845a98955dcac
chanwkkk <chanwk18@hotmail.com>
2020-05-27 21:26:34 -0600

    set up ejs and set the configuration

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require('path')
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", 'ejs');
+app.set("views", path.join(__dirname, './views'))
+
 app.use(express.static(path.join(__dirname, './public')))
 
 app.get("/", (req,res) =>{
