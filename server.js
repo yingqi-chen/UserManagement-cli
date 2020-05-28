0f93b1eeb697d0f81f24afc82e0f12db8eab50e6
chanwkkk <chanwk18@hotmail.com>
2020-05-27 21:35:25 -0600

    pass arguments to ejs template

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, './views'))
 app.use(express.static(path.join(__dirname, './public')))
 
 app.get("/", (req,res) =>{
-  res.render('pages/index.ejs')
+  res.render('pages/index.ejs', {message: "new ejs file!"})
 })
 
 app.listen(PORT, () =>{
